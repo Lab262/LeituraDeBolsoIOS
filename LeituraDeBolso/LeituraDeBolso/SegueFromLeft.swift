b192ab722c1518945a65bcf29c812907d1cad8bd
Huallyd <huallyd@gmail.com>
2016-09-22 18:49:26 -0300

    Button facebook position,

---
+++ LeituraDeBolso/LeituraDeBolso/SegueFromLeft.swift
@@ -0,0 +1,58 @@
+//
+//  SegueFromLeft.swift
+//  LeituraDeBolso
+//
+//  Created by Huallyd Smadi on 21/09/16.
+//  Copyright © 2016 Lab262. All rights reserved.
+//
+
+import UIKit
+
+class SegueFromLeft: UIStoryboardSegue {
+    
+    override func perform() {
+        
+//        let firstClassView = self.source.view
+//        let secondClassView = self.destination.view
+//        
+//        let screenWidth = UIScreen.main.bounds.size.width
+//        let screenHeight = UIScreen.main.bounds.size.height
+//        
+//        
+//         let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: screenWidth, height: screenHeight))
+//        
+//        secondClassView?.frame = rect
+//        
+//        if let window = UIApplication.shared.keyWindow {
+//            
+//            window.insertSubview(secondClassView!, aboveSubview: firstClassView!)
+//            
+//            UIView.animate(withDuration: 0.4, animations: { () -> Void in
+//                
+//                firstClassView?.frame = (firstClassView?.frame.offsetBy(dx: -screenWidth, dy: 0))!
+//                secondClassView?.frame = (secondClassView?.frame.offsetBy(dx: -screenWidth, dy: 0))!
+//                
+//            }) {(Finished) -> Void in
+//                
+//                self.source.navigationController?.pushViewController(self.destination, animated: false)
+//                
+//            }
+//            
+//        }
+        
+//        let src: UIViewController = self.source
+//        let dst: UIViewController = self.destination
+//        let transition: CATransition = CATransition()
+//        let timeFunc : CAMediaTimingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+//        transition.duration = 0.25
+//        transition.timingFunction = timeFunc
+//        transition.type = kCATransitionPush
+//        transition.subtype = kCATransitionFromLeft
+//        src.navigationController!.view.layer.add(transition, forKey: kCATransition)
+//        src.navigationController!.pushViewController(dst, animated: true)
+    }
+    
+    
+    
+
+}
